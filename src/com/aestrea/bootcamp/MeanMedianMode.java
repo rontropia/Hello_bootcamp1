9880cbfd722d15c4c256338565b19e6a242486f8
Rolianne Tropia <rontropia@aestrea.com>
2013-08-06 11:40:15 +0800

    Exer 1 Complete

--- src/com/aestrea/bootcamp/MeanMedianMode.java
+++ src/com/aestrea/bootcamp/MeanMedianMode.java
@@ -5,39 +5,28 @@ import java.util.Collections;
 import java.util.HashMap;
 
 public class MeanMedianMode {
-    /**
-     *
-     * Prints the mean, median and mode for a list of string arguments that are numbers
-     *
-     * @param args
-     */
+
     public void findMeanMedianMode(String[] args) {
-        ArrayList<Integer> userInput = new ArrayList<Integer>();
-        userInput = convertStringArgsToInteger(args);
-
-        System.out.println("Mean: "+findMean(userInput));
-        Collections.sort(userInput);
-        System.out.println("Median: "+findMedian(userInput));
-
-        System.out.println("Mode: "+findMode(userInput));
+        if (args.length > 0) {
+            ArrayList<Integer> userInput = new ArrayList<Integer>();
+            userInput = convertStringArgsToInteger(args);
+            Collections.sort(userInput);
+            System.out.println("Mean: " + findMean(userInput));
+            System.out.println("Median: " + findMedian(userInput));
+            System.out.println("Mode: " + findMode(userInput));
+        } else System.out.println("No Arguments Found!");
     }
-
-    /**
-     *
-     * @param userInput
-     * @return
-     */
 
     private static ArrayList findMode(ArrayList<Integer> userInput) {
         HashMap<Integer, Integer> seen = new HashMap();
         ArrayList<Integer> modeElements = new ArrayList<Integer>();
         int max = 0;
 
-        for(int i=0;i<userInput.size();i++){
-            if (seen.containsKey(userInput.get(i))){
-                seen.put(userInput.get(i), seen.get(userInput.get(i))+1);
+        for (int i = 0; i < userInput.size(); i++) {
+            if (seen.containsKey(userInput.get(i))) {
+                seen.put(userInput.get(i), seen.get(userInput.get(i)) + 1);
             } else {
-                seen.put(userInput.get(i),1);
+                seen.put(userInput.get(i), 1);
             }
             if (seen.get(userInput.get(i)) > max) {
                 max = seen.get(userInput.get(i));
@@ -53,30 +42,29 @@ public class MeanMedianMode {
     private int findMedian(ArrayList<Integer> userInput) {
         boolean isEven = (userInput.size() % 2) == 0;
 
-        if(isEven){
-            int upper = userInput.get(userInput.size()/2-1);
-            int lower = userInput.get(userInput.size()/2);
+        if (isEven) {
+            int upper = userInput.get(userInput.size() / 2 - 1);
+            int lower = userInput.get(userInput.size() / 2);
             return (upper + lower) / 2;
         } else {
-            return userInput.get((userInput.size()+1)/2-1);
+            return userInput.get((userInput.size() + 1) / 2 - 1);
         }
     }
 
     private int findMean(ArrayList<Integer> userInput) {
-        int mean;
         int sum = 0;
-        for(int j=0;j<userInput.size();j++){
-            sum += userInput.get(j);
+
+        for (Integer i : userInput) {
+            sum += i;
         }
-        mean = sum / userInput.size();
-        return mean;
+        return sum / userInput.size();
     }
 
     private ArrayList<Integer> convertStringArgsToInteger(String[] args) {
         ArrayList<Integer> temp = new ArrayList<Integer>();
-        if(args.length>0){
-            for(int i=0;i<args.length;i++){
-                temp.add(Integer.parseInt(args[i]));
+        if (args.length > 0) {
+            for (String s : args) {
+                temp.add(Integer.parseInt(s));
             }
         }
         return temp;
